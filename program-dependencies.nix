1908bea63bee6e319534a1c98487ab852d2bc8ea
keandrake-24 <keandre.imposter@gmail.com>
2025-11-30 14:21:57 +0700

    adding more pkgs and everything added swayconfiguration

--- program-dependencies.nix
+++ program-dependencies.nix
@@ -8,13 +8,33 @@
     # System packages
     environment.systemPackages = with pkgs; [
         # Add your program dependencies here, for example:
-        alacritty
-        waybar
-        nwg-look
-        git
+        alacritty #your terminal
+        waybar #your taskbar 
+        nwg-look #your gtk theme installer
+        lolcat # a must definitely
+        fastfetch # also a must
+        ranger # eh this is pretty good not necessary to have a TUI file manager
+        thunar #your file manager
         networkmanagerapplet
         google-chrome
+        noto-fonts
+        noto-fonts-cjk
+        noto-fonts-emoji
+        font-awesome
+        dunst
+        auto-cpufreq
     ];
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        pulse.enable = true; # PulseAudio compatibility
+    };
+    systemd.services.auto-cpufreq = {
+        enable = true;
+        serviceConfig = {
+            Restart = "always";
+        }
+    }
 
     # Enable the X server
     services.xserver.enable = true;
---
+++ swayconfiguration.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+    # Enable the sway window manager
+    services.sway = {
+        enable = true;
+        extraPackages = with pkgs; [
+            xorg.xwayland
+        ];
+    };
+
+    environment.systemPackages = [ rofi ];
+
+    # Enable xdg-desktop-portal for Wayland
+    services.xdg.portal = {
+        enable = true;
+        extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
+    };
+
+    # Optional: Set environment variables for Wayland
+    environment.variables = {
+        QT_QPA_PLATFORM = "wayland"; # Enable Wayland for Qt apps
+        XDG_CURRENT_DESKTOP = "sway";
+        XDG_SESSION_TYPE = "wayland";
+    };
+}
