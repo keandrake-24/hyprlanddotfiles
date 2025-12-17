e0e62dcefae6f9786163da6c240d74b94a59d728
keandrake-24 <keandre.imposter@gmail.com>
2025-12-17 18:33:07 +0700

    fixed actions.sh not showing askforsudo screen and fixed wallpaper script exit causing blank wallpaper

--- scripts/actions.sh
+++ scripts/actions.sh
@@ -3,10 +3,10 @@ options="Shutdown\nReboot\nSuspend\nExit Sway"
 choice=$(echo -e $options | rofi -dmenu -p "Choose an action"  )
 case  "$choice" in
 	Shutdown)
-		source askforsudo.sh | sudo -S shutdown now
+		source ~/.config/scripts/askforsudo.sh | sudo -S shutdown now
 		;;
 	Reboot)
-		source askforsudo.sh | sudo -S reboot
+		source ~/.config/scripts/askforsudo.sh | sudo -S reboot
 		;;
 	Suspend)
 		systemctl suspend
--- scripts/wallpapers.sh
+++ scripts/wallpapers.sh
@@ -5,7 +5,7 @@ current_wallpaper=$(cat ~/.config/sway/wallpaper)
 current_wallpaper=$(echo "$current_wallpaper" | awk '{print $4}')
 echo $current_wallpaper
 choice=$(echo -e $wallpapers | rofi -dmenu -p "Choose a wallpaper, current is $current_wallpaper")
-if [[ "$wallpapers" =~ "$choice" ]]; then
+if [[ "$wallpapers" =~ "$choice" && "$choice" != "" ]]; then
 sed -i "s|${current_wallpaper}|~/.config/wallpapers/${choice}|g" ~/.config/sway/wallpaper
 swaymsg reload
 fi
